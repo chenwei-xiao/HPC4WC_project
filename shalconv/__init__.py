b98b7c1a9e3727a2c824c257ad8a921ad1fa0557
Mefisto <stelliom@student.ethz.ch>
2020-07-12 21:01:52 +0200

    Changed int type

--- shalconv/__init__.py
+++ shalconv/__init__.py
@@ -4,7 +4,7 @@ from gt4py import gtscript
 BACKEND     = "gtx86"
 REBUILD     = True
 
-DTYPE_INT   = int
+DTYPE_INT   = np.int32
 DTYPE_FLOAT = np.float64
 INT_FIELD   = gtscript.Field[DTYPE_INT]
 FLT_FIELD   = gtscript.Field[DTYPE_FLT]
