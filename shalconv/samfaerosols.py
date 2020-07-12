ecc0015ad394ae2c2fba814b1c1fa4e12b2a1d97
Langwen Huang <huanglangwen@outlook.com>
2020-07-12 20:35:33 +0200

    aerosol finished

--- shalconv/samfaerosols.py
+++ shalconv/samfaerosols.py
@@ -1,8 +1,8 @@
-from .physcons import con_g, qamin
+from .physcons import con_g as g, qamin
 from . import DTYPE_FLOAT, BACKEND
 import gt4py as gt
 from gt4py import gtscript
-from __gtscript__ import PARALLEL, FORWARD, computation, interval
+from __gtscript__ import PARALLEL, FORWARD, BACKWARD, computation, interval
 import numpy as np
 
 ## Constants
@@ -16,7 +16,7 @@ def set_qaero(qtr, kmax, index_k):
 
 @gtscript.function
 def set_xmbp(xmb, delp):
-    xmbp = con_g * xmb / delp
+    xmbp = g * xmb / delp
     return xmbp
 
 @gtscript.function
@@ -108,7 +108,122 @@ def calc_ecko2_chem_c_dellae2(
             #for the subsidence term already is considered
             dellae2 = eta[0, 0, -1] * ecko2[0, 0, -1] * xmbp
 
+@gtscript.stencil(backend = BACKEND)
+def calc_dtime_max_arr(
+    delp: gtscript.Field[DTYPE_FLOAT],
+    ktcon: gtscript.Field[int],
+    index_k: gtscript.Field[int],
+    dtime_max_arr: gtscript.Field[DTYPE_FLOAT],
+    *,
+    delt: float
+):
+    with computation(FORWARD):
+        with interval(0, 1):
+            dtime_max_arr = delt
+        with interval(1, -1):
+            if index_k - 1 < ktcon:
+                dtime_max_arr = min(dtime_max_arr[0, 0, -1], 0.5 * delp[0, 0, -1])
+            else:
+                dtime_max_arr = dtime_max_arr[0, 0, -1]
 
+@gtscript.stencil(backend = BACKEND)
+def calc_detrainment_entrainment(
+    zi: gtscript.Field[DTYPE_FLOAT],
+    xlamue: gtscript.Field[DTYPE_FLOAT],
+    xlamud: gtscript.Field[DTYPE_FLOAT],
+    ecko2: gtscript.Field[DTYPE_FLOAT],
+    ctro2: gtscript.Field[DTYPE_FLOAT],
+    eta: gtscript.Field[DTYPE_FLOAT],
+    xmbp: gtscript.Field[DTYPE_FLOAT],
+    kb: gtscript.Field[int],
+    ktcon: gtscript.Field[int],
+    cnvflg: gtscript.Field[int],
+    index_k: gtscript.Field[int],
+    dellae2: gtscript.Field[DTYPE_FLOAT]
+):
+    with computation(PARALLEL), interval(1, -1):
+        if cnvflg and (index_k < ktcon):
+            dz   = zi[0, 0, 0] - zi[0, 0, -1]
+            aup  = 1.0 if index_k > kb else 0.0
+
+            dv1q = 0.5 * (ecko2[0, 0, 0] + ecko2[0, 0, -1])
+            dv2q = 0.5 * (ctro2[0, 0, 0] + ctro2[0, 0, -1])
+
+            tem  = 0.5 * (xlamue[0, 0, 0] + xlamue[0, 0, -1])
+            tem1 = 0.5 * (xlamud[0, 0, 0] + xlamud[0, 0,  0])
+
+            dellae2 = dellae2 + (
+                aup * tem1 * eta[0, 0, -1] * dv1q # detrainment from updraft
+              - aup * tem  * eta[0, 0, -1] * dv2q # entrainment into up and downdraft
+            ) * dz * xmbp
+
+            if index_k == kb:
+                dellae2 = dellae2 - eta * ctro2 * xmbp
+    with computation(FORWARD), interval(0, 1):
+        if cnvflg and (kb == 1):
+            dellae2 = dellae2 - eta * ctro2 * xmbp
+
+@gtscript.stencil(backend = BACKEND)
+def calc_mass_flux(
+    eta: gtscript.Field[DTYPE_FLOAT],
+    xmb: gtscript.Field[DTYPE_FLOAT],
+    qaero: gtscript.Field[DTYPE_FLOAT],
+    delp: gtscript.Field[DTYPE_FLOAT],
+    kb: gtscript.Field[int],
+    ktcon: gtscript.Field[int],
+    cnvflg: gtscript.Field[int],
+    index_k: gtscript.Field[int],
+    flx_lo: gtscript.Field[DTYPE_FLOAT],
+    totlout: gtscript.Field[DTYPE_FLOAT],
+    clipout: gtscript.Field[DTYPE_FLOAT],
+    dellae2: gtscript.Field[DTYPE_FLOAT],
+    *,
+    dtime_max: float
+):
+    with computation(PARALLEL), interval(1, -1):
+        if cnvflg and (index_k - 1 < ktcon):
+            tem = 0.0 if index_k - 1 < kb else eta[0, 0, -1]
+            # low-order flux, upstream
+            qaero_val = qaero[0, 0, 0] if tem > 0.0 else qaero[0, 0, -1]
+            flx_lo = - xmb * tem * qaero_val
+    # make sure low-ord fluxes don't violate positive-definiteness
+    with computation(PARALLEL), interval(0, -1):
+        if cnvflg and (index_k <= ktcon):
+            # time step / grid spacing
+            dtovdz = g * dtime_max / abs(delp)
+            # total flux out
+            totlout = max(0.0, flx_lo[0, 0, 1]) - min(0.0, flx_lo[0, 0, 0])
+            clipout = min(1.0, qaero / max(epsil, totlout) / (1.0001 * dtovdz))
+    # recompute upstream mass fluxes
+    with computation(PARALLEL), interval(1, -1):
+        if cnvflg and index_k - 1 <= ktcon:
+            tem = 0.0 if index_k - 1 < kb else eta[0, 0, -1]
+            clipout_val = clipout[0, 0, 0] if tem > 0.0 else clipout[0, 0, -1]
+            flx_lo = flx_lo * clipout_val
+    # a positive-definite low-order (diffusive) solution for the subsidnce fluxes
+    with computation(PARALLEL), interval(0, -1):
+        if cnvflg and index_k <= ktcon:
+            # time step / grid spacing
+            dtovdz = g * dtime_max / abs(delp)
+            dellae2 = dellae2 - (flx_lo[0, 0, 1] - flx_lo[0, 0, 0]) * dtovdz / dtime_max
+
+@gtscript.stencil(backend = BACKEND)
+def calc_final(
+    dellae2: gtscript.Field[DTYPE_FLOAT],
+    kmax: gtscript.Field[int],
+    ktcon: gtscript.Field[int],
+    cnvflg: gtscript.Field[int],
+    index_k: gtscript.Field[int],
+    qaero: gtscript.Field[DTYPE_FLOAT],
+    *,
+    delt: float
+):
+    # compute final aerosol concentrations
+    with computation(PARALLEL), interval(...):
+        if cnvflg and (index_k <= min(kmax, ktcon)):
+            qaero = qaero + dellae2 * delt
+            if qaero < 0.0:
+                qaero = qamin
 
 def samfshalcnv_aerosols(im, ix, km, itc, ntc, ntr, delt,
                          cnvflg, kb, kmax, kbcon, ktcon, fscav_np,
@@ -162,13 +277,15 @@ def samfshalcnv_aerosols(im, ix, km, itc, ntc, ntr, delt,
     #kmax_np = kmax[0, :, 0].view(np.ndarray)
     index_ijk_np = np.indices(shape_2d) 
     index_k = gt.storage.from_array(index_ijk_np[2] + 1, BACKEND, default_origin, shape_2d, dtype = int) # index STARTING FROM 1
+    dtime_max_arr = gt.storage.empty(BACKEND, default_origin, shape_2d, dtype = DTYPE_FLOAT)
 
     # Begin
     ## Check if aerosols are present
     if (ntc <= 0) or (itc <= 0) or (ntr <= 0): return
     if (ntr < itc + ntc - 3): return
 
-    # km1 = km - 1
+    calc_dtime_max_arr(delp, ktcon, index_k, dtime_max_arr, delt = delt)
+    dtime_max = dtime_max_arr[0, :, -1].view(np.ndarray).min()
 
     ## Tracer loop
     for n in range(ntc):
@@ -194,3 +311,17 @@ def samfshalcnv_aerosols(im, ix, km, itc, ntc, ntr, delt,
         calc_ecko2_chem_c_dellae2(zi, xlamue, xlamud, ctro2, c0t, eta, xmbp, kb,
                                    ktcon, cnvflg, index_k, ecko2, chem_c,
                                    dellae2, fscav = fscav)
+
+        # initialize maximum allowed timestep for upstream difference approach
+        # MOVED OUTSIDE OF TRACER LOOP
+
+        calc_detrainment_entrainment(zi, xlamue, xlamud, ecko2, ctro2, eta,
+                                     xmbp, kb, ktcon, cnvflg, index_k, dellae2)
+        
+        calc_mass_flux(eta, xmb, qaero, delp, kb, ktcon, cnvflg, index_k,
+                       flx_lo, totlout, clipout, dellae2, dtime_max = dtime_max)
+
+        calc_final(dellae2, kmax, ktcon, cnvflg, index_k, qaero, delt = delt)
+
+        qaero_np[:, :, n] = qaero.view(np.ndarray)[0, :, :]
+
